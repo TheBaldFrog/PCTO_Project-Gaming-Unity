6886071e4617ca7f18f89507d73fdff732b13bdc
TheBaldFrog <morarolivio@gmail.com>
2023-03-01 20:03:28 +0100

    aa

--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       //fregedrkjkjkjjljkljkjkgfsfdsfsdfbhjjkh
+       //fsdbfdgdsadasdasd
     }
 
     // Update is called once per frame
