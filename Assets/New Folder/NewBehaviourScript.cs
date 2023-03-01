1a280674f9fd198b1a03420a75e80a0b7db2c513
TheBaldFrog <morarolivio@gmail.com>
2023-03-01 20:14:48 +0100

    add script

--- Assets/New Folder/NewBehaviourScript.cs
+++ Assets/New Folder/NewBehaviourScript.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       //fregedrkjkjkjjljkljkjkgfsfdsfsdfbhjjkh
-       //fsdbfdgdsadasdasdvcxvxcvxc
+        
     }
 
     // Update is called once per frame
