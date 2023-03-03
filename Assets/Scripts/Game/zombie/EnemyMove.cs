d9ecbbd1afe2e44d834264a19b834b8be7de7ff1
TheBaldFrog <morarolivio@gmail.com>
2023-03-04 00:28:26 +0100

    gg

--- Assets/Scripts/Game/zombie/EnemyMove.cs
+++ Assets/Scripts/Game/zombie/EnemyMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    //private Player player;
+
     // Start is called before the first frame update
     void Start()
     {
